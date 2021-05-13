2dfcce5527c0c3d978097a551e189ffa94e15d84
berdysh-dev <berdysh.dev@gmail.com>
2021-05-14 02:02:39 +0900

    t2

--- test.php
+++ test.php
@@ -1,4 +1,4 @@
 <?php
     require_once "vendor/autoload.php";
     $helloworld = new berdysh_dev\helloworld('HELLO WORLD');
-    printf("%s\n",$helloworld->getter()) ;
+    printf("%s\n\n",$helloworld->getter()) ;
